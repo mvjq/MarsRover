f9f8c335abea5e2069a1f4987a6312b3478d02c0
Marcos Vinicius Junqueira <mviniciusjunqueira@gmail.com>
2022-10-10 14:51:44 -0300

    Init repository and spring initializer template

---
+++ src/test/java/com/elo7/marsrover/MarsRoverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elo7.marsrover;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarsRoverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

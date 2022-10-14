dbd5dadfb9095de5ff58ccc7484b9acc19c2168f
Marcos Vinicius Junqueira <mviniciusjunqueira@gmail.com>
2022-10-14 15:22:58 -0300

    updated planet repository
    Signed-off-by: Marcos Vinicius Junqueira <mviniciusjunqueira@gmail.com>

--- src/main/java/com/elo7/marsrover/repository/PlanetRepository.java
+++ src/main/java/com/elo7/marsrover/repository/PlanetRepository.java
@@ -1,13 +1,16 @@
 package com.elo7.marsrover.repository;
 
 import com.elo7.marsrover.model.Planet;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
+
 public interface PlanetRepository extends JpaRepository<Planet, Integer> {
+    @EntityGraph(type = EntityGraph.EntityGraphType.FETCH,
+            value =  "planet-rover-graph")
     Optional<Planet> findPlanetByPlanetName(String planetName);
-    void deleteByPlanetName(String planetName);
 }
